d8b0fccc73b1837c3d8c2f9511c5deccfff50702
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-11 12:05:08 +0530

    Backend - created subTask model

---
+++ Phase-1/Mega Project/backend/src/models/subtask.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { mongo } from "mongoose";
+
+export interface subTaskDocument extends mongoose.Document {
+  title: string;
+  task: mongoose.Types.ObjectId;
+  isCompleted: boolean;
+  createdBy: mongoose.Types.ObjectId;
+  createAt: Date;
+  updatedAt: Date;
+}
+const subtaskSchema = new mongoose.Schema<subTaskDocument>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    task: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const subTask = mongoose.model<subTaskDocument>("SubTask", subtaskSchema);
+export default subTask;

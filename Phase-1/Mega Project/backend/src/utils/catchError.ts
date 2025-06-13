f1e6009a876cf70f306650dce5a8506fc25b037b
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-13 12:02:54 +0530

    Revert "Backend - modified catchError funtion"
    This reverts commit 4f5600bfb4980696208ed9cd4b208f9a617ec525.

--- Phase-1/Mega Project/backend/src/utils/catchError.ts
+++ Phase-1/Mega Project/backend/src/utils/catchError.ts
@@ -6,22 +6,14 @@ type AsyncController = (
   next: NextFunction,
 ) => Promise<any>;
 
-// const catchError =
-//   (controller: AsyncController): AsyncController =>
-//   async (req, res, next) => {
-//     try {
-//       await controller(req, res, next);
-//     } catch (error) {
-//       next(error);
-//     }
-//   };
-
-// export default catchError;
-
-export function asyncHandler(requestHandler: AsyncController) {
-  return function (req: Request, res: Response, next: NextFunction) {
-    Promise.resolve(requestHandler(req, res, next)).catch(function (err) {
-      next(err);
-    });
+const catchError =
+  (controller: AsyncController): AsyncController =>
+  async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
   };
-}
+
+export default catchError;

16601ec1fd16e2952438a17d14512341b348c765
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-14 10:23:48 +0530

    Backend - created healthCheck api

--- Projects/Kanban-board/backend/src/index.ts
+++ Projects/Kanban-board/backend/src/index.ts
@@ -1,8 +1,26 @@
 import "dotenv/config";
 import express from "express";
 import connectToDatabase from "./config/db";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { APP_ORIGIN } from "./constants/env";
+import { OK } from "./constants/statusCode";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use("/health", (req, res) => {
+  res.status(OK).json({ message: "Server is running" });
+});
 
 app.listen(3000, () => {
   console.log(`Server running on PORT 3000`);
